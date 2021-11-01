adf726ff0c3130d5fb2aab74871bdfc9645e0c6f
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-10-31 22:18:18 -0500

    beginning admin part

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.db.models import Q, F
+from django.contrib.contenttypes.models import ContentType
+from store.models import Collection, OrderItem, Product, Order
+from tags.models import TaggedItem
+from django.db import transaction
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # ordered_items_unique = OrderItem.objects.values('product_id').distinct()
+    # products = Product.objects.filter(id__in=ordered_items_unique).order_by('title')
+    # orders = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # tags = TaggedItem.objects.get_tags_for(Product, 1)
+    # with transaction.atomic():
+    #     collection = Collection.objects.get(pk=11)
+    #     collection.featured_product = None
+    #     collection.save()
+    queryset = Product.objects.all()
+    return render(request, "hello.html", {"name": "Mosh", "tags": list(queryset)})
